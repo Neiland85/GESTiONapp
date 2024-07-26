80358d8882b47b365a477f4ca57196d8c071a3da
Neiland85 <bookingnadarecords@gmail.com>
2024-07-27 01:56:21 +0200

    Add test file for file upload tests & test_processor file uploaded

--- tests/test_file_processor.py
+++ tests/test_file_processor.py
@@ -2,13 +2,23 @@
 import unittest
 from fastapi.testclient import TestClient
 from main import app
+import os
 
 client = TestClient(app)
 
 class TestFileUpload(unittest.TestCase):
     def test_upload_files(self):
-        with open("test_file.pdf", "rb") as file:
+        # Ruta del archivo de prueba
+        test_file_path = os.path.join(os.path.dirname(__file__), "test_file.pdf")
+        
+        # Asegurarse de que el archivo existe
+        self.assertTrue(os.path.exists(test_file_path), "Test file does not exist")
+
+        # Abrir el archivo de prueba y realizar la solicitud
+        with open(test_file_path, "rb") as file:
             response = client.post("/upload/", files={"files": file})
+        
+        # Verificar la respuesta
         self.assertEqual(response.status_code, 200)
         self.assertIn("Files successfully uploaded", response.json()["message"])
 
