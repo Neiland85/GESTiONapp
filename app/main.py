064d8c2f7ec9994962b94d0eb7117a5c63a85335
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-07-27 06:23:40 +0200

    Update main.py
    Add config main.

--- app/main.py
+++ app/main.py
@@ -5,10 +5,7 @@ from config import config_by_name
 def create_app(config_name='dev'):
     app = FastAPI()
 
-    # Configuración de la aplicación
     app.config = config_by_name[config_name]
-
-    # Registrar los routers
     app.include_router(views_router, prefix="/auth")
 
     return app
