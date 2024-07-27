fe76612036684712028ef900488966ce56b6ea87
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-07-27 06:18:18 +0200

    Update test_auth.py
    Update test auth.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,40 +1,22 @@
 import unittest
 from fastapi.testclient import TestClient
-from main import app
-from app.db import fake_users_db, get_password_hash
-
-client = TestClient(app)
+from app.main import create_app
 
 class TestAuth(unittest.TestCase):
     def setUp(self):
-        self.test_user = {
-            "username": "testuser",
-            "password": "testpassword"
-        }
-        self.test_admin = {
-            "username": "testadmin",
-            "password": "testpassword",
-            "role": "admin"
-        }
-        hashed_password = get_password_hash(self.test_user["password"])
-        fake_users_db[self.test_user["username"]] = {
-            "username": self.test_user["username"],
-            "hashed_password": hashed_password,
-            "role": "user"
-        }
+        self.app = TestClient(create_app())
 
-def test_user_registration(self):
-    response = self.client.post("/auth/register", json={"username": "testuser", "password": "testpass"})
-    self.assertEqual(response.status_code, 200)
+    def test_user_registration(self):
+        response = self.app.post("/auth/register/", json={"username": "testuser", "password": "testpass"})
+        self.assertEqual(response.status_code, 200)
 
-def test_user_login(self):
-    response = self.client.post("/auth/login", json={"username": "testuser", "password": "testpass"})
-    self.assertEqual(response.status_code, 200)
+    def test_user_login(self):
+        response = self.app.post("/auth/token", data={"username": "testuser", "password": "testpass"})
+        self.assertEqual(response.status_code, 200)
 
-def test_invalid_user_login(self):
-    response = self.client.post("/auth/login", json={"username": "wronguser", "password": "wrongpass"})
-    self.assertEqual(response.status_code, 400)
+    def test_invalid_user_login(self):
+        response = self.app.post("/auth/token", data={"username": "wronguser", "password": "wrongpass"})
+        self.assertEqual(response.status_code, 400)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
