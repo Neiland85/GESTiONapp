5b94a54c2416f54a11c5fb5ec9e601194e198346
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-07-27 06:09:39 +0200

    Update models.py
    Update user models tokens.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
-from typing import Optional
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 class User(BaseModel):
     username: str
@@ -8,12 +11,3 @@ class User(BaseModel):
 class UserInDB(User):
     hashed_password: str
     role: str
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
-    role: Optional[str] = None
-
