a112cdb4f8d8e0f7469e51f4b7195ffec04f3553
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-07-27 06:22:44 +0200

    Update main.py
    Add configuration dependencies.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from .views import router as views_router
+from config import config_by_name
 
-def create_app():
+def create_app(config_name='dev'):
     app = FastAPI()
-   
+
+    # Configuración de la aplicación
+    app.config = config_by_name[config_name]
+
+    # Registrar los routers
     app.include_router(views_router, prefix="/auth")
 
     return app
