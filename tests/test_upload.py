a7807741088eee8b9ec710981b1986914485ffbc
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-07-27 05:18:43 +0200

    Update test_upload.py
    Add update test files.

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -6,20 +6,19 @@ client = TestClient(app)
 
 class TestFileUpload(unittest.TestCase):
     def test_upload_jpg(self):
-        with open("test_files/test.jpg", "rb") as file:
+        with open("tests/test_files/test.jpg", "rb") as file:
             response = client.post("/upload/", files={"files": file})
             self.assertEqual(response.status_code, 200)
 
     def test_upload_pdf(self):
-        with open("test_files/test.pdf", "rb") as file:
+        with open("tests/test_files/test.pdf", "rb") as file:
             response = client.post("/upload/", files={"files": file})
             self.assertEqual(response.status_code, 200)
 
     def test_upload_png(self):
-        with open("test_files/test.png", "rb") as file:
+        with open("tests/test_files/test.png", "rb") as file:
             response = client.post("/upload/", files={"files": file})
             self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
-
